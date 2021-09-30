b528b9185d0729d96b7ff4af32269c1d4fd0f806
Ryan <49902829+ryantanrk@users.noreply.github.com>
2021-09-30 21:47:54 +0800

    navbar done

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,34 +1,41 @@
 import "../css/TopBar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function TopBar() {
     return (
-        <div className="topbar">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid">
-                    <span className="navbar-brand">Popcorn Info</span>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
+        <div class="topbar">
+            <nav class="navbar navbar-expand-lg navbar-dark">
+                <div class="container-fluid">
+                    <span class="navbar-brand">Popcorn Info</span>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                        <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="nav">
-                            <li className="nav-item">
-                                <button className="btn nav-link">Trending</button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <button class="btn nav-link">Trending</button>
                             </li>
-                            <li className="nav-item">
-                                <button className="btn nav-link">Browse</button>
+                            <li class="nav-item">
+                                <button class="btn nav-link">Browse</button>
                             </li>
-                            <li className="nav-item dropdown">
-                                <button className="btn nav-link dropdown-toggle" id="fydropdown" href="#" data-bs-toggle="dropdown" aria-expanded="false">For You</button>
+                            <li class="nav-item dropdown">
+                                <button class="btn nav-link dropdown-toggle" id="fydropdown" href="#" data-bs-toggle="dropdown" aria-expanded="false">For You</button>
+                                <ul class="dropdown-menu" aria-labelledby="fydropdown">
+                                    <button class="btn dropdown-item">Favorites</button>
+                                    <button class="btn dropdown-item">Watch Later</button>
+                                    <hr class="dropdown-divider"/>
+                                    <button class="btn dropdown-item">Reviews</button>
+                                </ul>
                             </li>
-                            <div className="dropdown-menu" aria-labelledby="fydropdown">
-                                <button className="btn dropdown-item">Favorites</button>
-                                <button className="btn dropdown-item">Watch Later</button>
-                                <hr className="dropdown-divider"/>
-                                <button className="btn dropdown-item">Reviews</button>
-                            </div>
+                            
                         </ul>
-                        <form className="d-flex"></form>
+                        <form class="d-flex">
+                            <input class="form-control me-2" type="search" placeholder="Search movies..." aria-label="Search"/>
+                            <button class="btn btn-outline-success me-2" type="submit"><FontAwesomeIcon icon={ faSearch }/></button>
+                        </form>
+                        <button class="btn btn-success">Account</button>
                     </div>
                 </div>
             </nav>
